ea45586c5faaeda840e1fd40dc982384339b3cda
Marcus Willock <crazcalm@gmail.com>
2014-08-28 22:43:31 -0400

    I now have github login

--- chatapp/client/client.js
+++ chatapp/client/client.js
@@ -8,7 +8,14 @@ Template.messages.messages = function () {
 Template.input.events = {
   'keydown input#message' : function (event) {
     if (event.which ==13) {// 13 is the enter key event
-      var name = "Anonymous";
+
+      if (Meteor.user()) {
+        var name = Meteor.user().profile.name;
+      }
+      else {
+        var name = "Anonymous";
+      }
+
       var message = document.getElementById('message');
 
       if (message.value != "") {
